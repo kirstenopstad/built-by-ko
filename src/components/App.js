fd46d4845ff76406323cac1d6e095f6952ee6143
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-02-23 12:00:48 -0800

    add placeholder PortfolioContorl for logged in in admin

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Header from '../components/Header'
 import Profile from '../components/Profile'
 import Portfolio from '../components/Portfolio'
+import PortfolioControl from '../components/PortfolioControl'
 import Container from 'react-bootstrap/Container';
 import SignIn from "./SignIn";
+import { auth } from "./../firebase.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
@@ -18,12 +20,23 @@ function App() {
         <Portfolio />
       </div>
     </Container>
-
   </React.Fragment>
+  let boh = null;
+
+  // TODO: Make this specific to my UID
+  if (auth.currentUser != null) {
+    boh = <PortfolioControl />
+  } else {
+    boh = <SignIn />
+  }
+
+    
+
   return (
     <Router>
       <Routes>
-      <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/update-portfolio" element={boh} />
         <Route path="/" element={site} />
       </Routes>
     </Router>
