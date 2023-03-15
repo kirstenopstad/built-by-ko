1a2954f12fa93d9f63feae14658cac93f0dc9f8d
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-15 15:02:37 -0700

    add confirm delete functionality and modal styling

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const AllProjects = ({portfolio, handleAddProjectClick, handleEditProjectClick, handleDeleteProject}) => {
+  const [show, setShow] = useState(false);
+  const [deleteProject, setDeleteProject] = useState({title: "title",id: 1});
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = (project) => {
+    setShow(true);
+    setDeleteProject(project);
+  }
+
+  const handleDeleteConfirm = () => {
+    handleDeleteProject(deleteProject);
+    setDeleteProject({title: "title",id: 1});
+    setShow(false);
+  }
+  
   return(
     <div>
       <h1>All Projects</h1>
@@ -24,12 +41,28 @@ const AllProjects = ({portfolio, handleAddProjectClick, handleEditProjectClick, 
             <td>
               <Button onClick={() => handleEditProjectClick(project.id)} variant="outline-success">Edit</Button>
               {' '}
-              <Button onClick={() => handleDeleteProject(project)} variant="outline-danger">Delete</Button>
+              <Button onClick={() => handleShow(project)} variant="outline-danger">Delete</Button>
+              {/* <Button onClick={() => handleDeleteProject(project)} variant="outline-danger">Delete</Button> */}
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Confirm Project Delete</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to delete {deleteProject.title}?</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="danger" onClick={handleDeleteConfirm}>
+                    Delete
+                  </Button>
+                </Modal.Footer>
+              </Modal>
             </td>
           </tr>
         )}
         </tbody>
       </Table>
+      
       <Button onClick={handleAddProjectClick} variant="success">Add Project</Button>
     </div>
   )
