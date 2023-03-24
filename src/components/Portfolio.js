f04a0c5b3242ac663efef9cc1cd975a9c2043d41
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-03-24 14:33:31 -0700

    add filter functionality for jS, react, c# and python, distinctly

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,17 +6,15 @@ import Button from 'react-bootstrap/Button';
 const Portfolio = ({projectList}) => {
   const [showJavaScript, setShowJavaScript] = useState(false);
   const [showReact, setShowReact] = useState(false);
-  // const [showDotNet, setShowDotNet] = useState(false);
-  // const [showPython, setShowPython] = useState(false);
-  
-  // TECH
-  const jS =  "JavaScript";
-  const react =  "React";
+  const [showDotNet, setShowDotNet] = useState(false);
+  const [showPython, setShowPython] = useState(false);
 
   // TECH LIST
   const techObject = {
     'JavaScript': showJavaScript,
     'React': showReact,
+    'C#': showDotNet,
+    'Python': showPython
   }
 
   const getTechSelected = () => {
@@ -45,35 +43,6 @@ const Portfolio = ({projectList}) => {
   }
 
   const techSelected = getTechSelected(techObject)
-  console.log(techSelected)
-  console.log(getTechSelected(techObject))
-
-  // const getTechSelected = (techObject) => {
-  //   let techSelected = [];
-  //   Object.entries(techObject).forEach(([key, value]) => { 
-  //     const tech = key;
-  //     const showState = value;
-  //     getTechSelected(value, key)
-  //     if (showState) {
-  //       // if show tech is TRUE, add tech to techSelected
-  //       console.log(`${techSelected}`)
-  //       return [...techSelected, tech];
-  //     } else {
-  //       // if show tech is FALSE, remove from techSelected
-  //       const index = techSelected.indexOf(tech);
-  //       if (index >= 0) {
-  //         const techList = [...techSelected];
-  //         techList.splice(index, 1)
-  //         console.log(`${techList}`)
-  //         return techList;
-  //       } else {
-  //         console.log(`${techSelected}`)
-  //         return techSelected
-  //       }
-  //     }
-  //   }) 
-  // }
-
 
   // handle click functions
   const handleJSClick = () => {
@@ -84,21 +53,20 @@ const Portfolio = ({projectList}) => {
     setShowReact(!showReact);
   }
 
+  const handleDotNetClick = () => {
+    setShowDotNet(!showDotNet);
+  }
+
+  const handlePythonClick = () => {
+    setShowPython(!showPython);
+  }
+
   // conditional button rendering
   // result = condition ? true : false
   const JSButtonVariant = showJavaScript ? "secondary" : "outline-secondary";
   let ReactButtonVariant = showReact ? "secondary" : "outline-secondary";
-  
-
-  // conditional filtering if techSelected changes
-  // useEffect(() => { 
-  //   let filteredProjs = [];
-  //   techSelected.forEach((tech) => {
-  //     // copy in projects from previous step + add new tech
-  //     filteredProjs = [...filteredProjs, projectList.filter(p => p.techUsed.includes(tech))]
-  //   })
-  //   setFilteredProjects(filteredProjs)
-  // }, [techSelected])
+  let DotNetButtonVariant = showDotNet ? "secondary" : "outline-secondary";
+  let PythonButtonVariant = showPython ? "secondary" : "outline-secondary";
 
   // if anything is selected, filter from 
   let filteredProjects = [];
@@ -108,15 +76,6 @@ const Portfolio = ({projectList}) => {
       filteredProjects = [...filteredProjects, projectList.filter(p => p.techUsed.includes(tech))]
     })
   }
-
-  // conditionally filter project list
-  // let filteredProjects = [];
-  // if (showJavaScript) {
-  //   filteredProjects = [...filteredProjects, projectList.filter(p => p.techUsed.includes(jS))]
-  // } 
-  // if (showReact) {
-
-  // }
 
   // conditionally display either filtered or non-filtered project list
   if (techSelected.length > 0) {
@@ -132,8 +91,8 @@ const Portfolio = ({projectList}) => {
         <h2 className="section">Portfolio</h2>
         <Button variant={JSButtonVariant} size="sm" onClick={handleJSClick}>JavaScript</Button>
         <Button variant={ReactButtonVariant} size="sm" onClick={handleReactClick}>React</Button>
-        <Button variant="outline-secondary" size="sm">.Net</Button>
-        <Button variant="outline-secondary" size="sm">Python</Button>
+        <Button variant={DotNetButtonVariant} size="sm" onClick={handleDotNetClick}>.Net</Button>
+        <Button variant={PythonButtonVariant} size="sm" onClick={handlePythonClick}>Python</Button>
         {' '}
         <List portfolio={projectList}/>
       {/* <Detail /> */}
