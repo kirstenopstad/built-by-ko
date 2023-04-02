c36f8c131961a0da276a2b4f903e11824ee801ca
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-04-02 11:40:36 -0700

    edit bio to make concise

--- src/profileSeedData.js
+++ src/profileSeedData.js
@@ -7,7 +7,7 @@ import calendarIcon from './img/icons/calendar3-event.svg'
 const Profile = {
   background: [`Hi! I'm Kirsten. I'm passionate about building things that solve real-world problems.`,
     `Bringing fifteen years of design experience to a new chapter of my career, my specialty is translating complex techincal concepts to collaborators so that the whole team can weigh trade-offs and make smart, sustainable choices together. I have a proven track record of taking projects from conceptual ideas to technical deliverables.`,
-    `After years of developing web products as side projects, I am now focused on codifying & intentionally applying those technical skills as a central part of my career. I've earned certifications in Computer Science and Full Stack Software Engineering – and continue to actively grow those skills every week.`,
+    // `After years of developing web products as side projects, I am now focused on codifying & intentionally applying those technical skills as a central part of my career. I've earned certifications in Computer Science and Full Stack Software Engineering – and continue to actively grow those skills every week.`,
     `As a huge fan of Brene Brown's work around "living into our values," in the workplace and in one's personal life; and, in the spirit of vulnerability, I share my core values to give a sense of who I am and what's important to me:`,
     `1. Intentionality`,
     `2. Authenticity`,
@@ -26,13 +26,17 @@ const Profile = {
   "Reading Poetry & Fiction"],
 
   skills:
-    ["HTML & CSS",
-    "Git Workflow",
-    "C# | ASP.Net Core",
-    "JavaScript | React",
+    [
+    "Project Management",
     "Full Stack Engineering",
     "Detailed Documentation",
-    "Project Management"],
+    "Git Workflow",
+    "C# | ASP.Net Core 6",
+    "JavaScript | React",
+    "Python | Flask",
+    "SQL | NoSQL",
+    "HTML & CSS",
+    ],
 
   links: {
     github: {
